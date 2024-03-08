814e276c7bcb7fd7cdb4ef91d7e661def45c2854
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2024-03-08 01:03:30 -0600

    proyecto lanzado al tomcat

--- src/java/controller/ServletControlador.java
+++ src/java/controller/ServletControlador.java
@@ -5,10 +5,31 @@
  */
 package controller;
 
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import model.Rectangulo;
 /**
  *
  * @author EMMANUEL
  */
-public class ServletControlador {
+
+@WebServlet("/ServletControlador")
+public class ServletControlador extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest request , HttpServletResponse response) throws ServletException, IOException{
+        Rectangulo rec = new Rectangulo(3,6);
+        
+        request.setAttribute("mensaje", "Saludos desde el Servlet");
+        
+        HttpSession sesion = request.getSession();
+        sesion.setAttribute("rectangulo", rec);
+        
+        RequestDispatcher rd = request.getRequestDispatcher("vista/desplegarVariables.jsp");
+        rd.forward(request, response);
+    }
     
 }
---
+++ web/vista/desplegarVariables.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : desplegarVariables
+    Created on : 8/03/2024, 01:00:50 AM
+    Author     : EMMANUEL
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1> Despliegue de Variables </h1>
+        Variables en alcance request: ${mensaje}
+    </body>
+</html>
